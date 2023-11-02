c814c0233b99ced1cae1c6e8fa14b244aad5f6e9
fnandesrafael <fnandesrafael@pm.me>
2023-11-02 16:50:00 -0300

    feat: add daisy-ui library to the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -15,6 +15,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  // eslint-disable-next-line global-require
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['retro'],
+  },
+};
+export default config;
