dba177b7f4908d4563680d4b13259eacad8830b2
fnandesrafael <fnandesrafael@pm.me>
2023-11-03 16:08:38 -0300

    feat: add responsivity to tablet screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,33 +7,34 @@ import Display from '@/components/Display';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start overflow-x-hidden bg-base-100 font-sans font-medium">
+    <>
       <Navigator.Root>
         <Navigator.Navbar />
         <Navigator.Dropdown />
       </Navigator.Root>
+      <main className="flex min-h-screen flex-col items-center justify-start overflow-x-hidden bg-base-100 px-[4vw] font-sans font-medium">
+        <HeroSection />
 
-      <HeroSection />
+        <section className="container mb-8 mt-48 md:mt-52">
+          <div className="flex flex-col">
+            <h1 className="z-10 mb-2 font-head text-3xl uppercase md:text-5xl">
+              Heróis
+            </h1>
+            <p className="z-10 mb-6 text-sm opacity-75 md:text-base">
+              Pesquise seu herói favorito digitando no campo abaixo.
+            </p>
+            <Image
+              src={gridElement}
+              alt=""
+              className="absolute left-0 mb-96 hidden w-8 md:flex"
+            />
 
-      <section className="container mb-8 mt-48 px-[4vw] md:mt-52">
-        <div className="flex flex-col">
-          <h1 className="z-10 mb-2 font-head text-3xl uppercase md:text-5xl">
-            Heróis
-          </h1>
-          <p className="z-10 mb-6 text-sm opacity-75 md:text-base">
-            Pesquise seu herói favorito digitando no campo abaixo.
-          </p>
-          <Image
-            src={gridElement}
-            alt=""
-            className="absolute left-0 mb-96 hidden w-8 md:flex"
-          />
+            <SearchBar />
+          </div>
 
-          <SearchBar />
-        </div>
-
-        <Display />
-      </section>
-    </main>
+          <Display />
+        </section>
+      </main>
+    </>
   );
 }
--- src/components/Navigator/fragments/Root.tsx
+++ src/components/Navigator/fragments/Root.tsx
@@ -6,7 +6,7 @@ type NavigatorProps = {
 
 export default function Navigator({ children }: NavigatorProps) {
   return (
-    <header className="flex w-full flex-row justify-end p-4 md:p-4 md:px-12">
+    <header className="flex w-full flex-row justify-end bg-base-100 p-4 font-sans md:p-4 md:px-12">
       {children}
     </header>
   );
