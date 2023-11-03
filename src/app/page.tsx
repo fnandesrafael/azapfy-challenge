59cecee426b5682c11ff7bf5c7ee4d608872d8e5
fnandesrafael <fnandesrafael@pm.me>
2023-11-03 10:44:38 -0300

    refact: adding more componentization to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image';
+import Hero from '@/components/Hero';
 import Navigator from '@/components/Navigator';
-import heroImage from '../../public/images/heroimage.png';
 
 export default function Home() {
   return (
@@ -10,30 +9,7 @@ export default function Home() {
         <Navigator.Dropdown />
       </Navigator.Root>
 
-      <section className="container mt-14 flex flex-row justify-between px-[5vw]">
-        <div className="flex max-w-xl flex-col">
-          <h1 className="mb-8 font-head text-8xl uppercase">
-            Jornada do Herói
-          </h1>
-          <p className="mb-24 max-w-lg opacity-75">
-            Ponha seus heróis favoritos em uma batalha justa e deixe que vença o
-            melhor!
-          </p>
-
-          <div className="flex flex-row items-start gap-4">
-            <button className="btn btn-primary">Jogar</button>
-            <button className="btn btn-secondary">Sobre</button>
-          </div>
-        </div>
-
-        <div>
-          <Image
-            src={heroImage}
-            alt="Imagem de um menino em modelagem 3D correndo com uma capa de super herói."
-            className="absolute right-0 top-20 w-[600px]"
-          />
-        </div>
-      </section>
+      <Hero />
     </main>
   );
 }
