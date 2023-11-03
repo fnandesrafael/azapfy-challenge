a88c4cdcac211563dcd90bccfea99e58613f2d46
fnandesrafael <fnandesrafael@pm.me>
2023-11-03 15:02:52 -0300

    fix: grid element in mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,17 @@ export default function Home() {
 
       <section className="container mb-8 mt-48 px-[4vw] md:mt-52">
         <div className="flex flex-col">
-          <h1 className="mb-2 font-head text-3xl uppercase md:text-5xl">
+          <h1 className="z-10 mb-2 font-head text-3xl uppercase md:text-5xl">
             Heróis
           </h1>
-          <p className="mb-8 text-sm opacity-75 md:text-base">
+          <p className="z-10 mb-6 text-sm opacity-75 md:text-base">
             Pesquise seu herói favorito digitando no campo abaixo.
           </p>
-          <Image src={gridElement} alt="" className="absolute left-0 w-8" />
+          <Image
+            src={gridElement}
+            alt=""
+            className="absolute left-0 mb-96 hidden w-8 md:flex"
+          />
 
           <SearchBar />
         </div>
