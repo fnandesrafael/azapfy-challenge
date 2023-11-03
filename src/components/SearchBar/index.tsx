c483a239f14059893e205e46a8b6b6b5414f54bc
fnandesrafael <fnandesrafael@pm.me>
2023-11-03 14:26:11 -0300

    SearchBar button style corrections

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BiSearch } from 'react-icons/bi';
 
 export default function SearchBar() {
   return (
@@ -9,21 +10,8 @@ export default function SearchBar() {
         className="input input-bordered w-full text-sm transition-shadow duration-200 ease-in-out placeholder:text-sm placeholder:text-[#16160a20] focus:shadow-comic focus:outline-none md:w-1/2 md:text-sm md:placeholder:text-sm"
         role="search-input"
       />
-      <button className="btn btn-primary rounded-full shadow-comic hover:shadow-comic md:shadow-none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+      <button className="btn btn-primary h-12 w-12 rounded-full shadow-comic md:shadow-none md:hover:shadow-comic">
+        <BiSearch className="text-2xl" />
       </button>
     </form>
   );
