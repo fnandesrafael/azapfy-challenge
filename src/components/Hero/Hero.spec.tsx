80dcb529208dd1208614b0b963a4bf74ad20ace9
fnandesrafael <fnandesrafael@pm.me>
2023-11-03 11:09:46 -0300

    feat: Hero component test cases

---
+++ src/components/Hero/Hero.spec.tsx
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import Hero from '.';
+
+describe('<Hero /> component', () => {
+  it('should be in the document', () => {
+    const { getByRole } = render(<Hero />);
+
+    const sut = getByRole('hero');
+
+    expect(sut).toBeInTheDocument();
+  });
+
+  it('should have both heading and paragraph with matching texts', () => {
+    const { getByRole } = render(<Hero />);
+
+    const [sutA, sutB] = [
+      getByRole('heading', { level: 1 }),
+      getByRole('paragraph'),
+    ];
+
+    expect(sutA).toBeInTheDocument();
+    expect(sutA).toHaveTextContent(/jornada do herói/i);
+
+    expect(sutB).toBeInTheDocument();
+    expect(sutB).toHaveTextContent(
+      /Ponha seus heróis favoritos em uma batalha justa e deixe que vença o melhor!/i,
+    );
+  });
+
+  it('should have two buttons with matching texts', () => {
+    const { getAllByRole } = render(<Hero />);
+
+    const sut = getAllByRole('button');
+
+    expect(sut.length).toBe(2);
+
+    expect(sut[0]).toHaveTextContent(/jogar!/i);
+    expect(sut[1]).toHaveTextContent(/sobre/i);
+  });
+
+  it('should have an image', () => {
+    const { getByRole } = render(<Hero />);
+
+    const sut = getByRole('img');
+
+    expect(sut).toBeInTheDocument();
+  });
+});
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,16 +4,19 @@ import heroImage from '../../../public/images/heroimage.png';
 
 export default function Hero() {
   return (
-    <section className="container mt-14 flex flex-row justify-between px-[5vw]">
+    <section
+      className="container mt-14 flex flex-row justify-between px-[5vw]"
+      role="hero"
+    >
       <div className="flex max-w-xl flex-col">
         <h1 className="mb-8 font-head text-8xl uppercase">Jornada do Herói</h1>
-        <p className="mb-24 max-w-lg opacity-75">
+        <p className="mb-24 max-w-lg opacity-75" role="paragraph">
           Ponha seus heróis favoritos em uma batalha justa e deixe que vença o
           melhor!
         </p>
 
         <div className="flex flex-row items-start gap-4">
-          <button className="btn btn-primary">Jogar</button>
+          <button className="btn btn-primary">Jogar!</button>
           <button className="btn btn-secondary">Sobre</button>
         </div>
       </div>
