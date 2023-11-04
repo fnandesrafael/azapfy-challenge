353f45b23713bc175ddce33091839a5257c4d98c
fnandesrafael <fnandesrafael@pm.me>
2023-11-04 12:32:58 -0300

    feat: Heroes filter

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
-import { BiSearch } from 'react-icons/bi';
+'use client';
+
+import React, { useRef, useState } from 'react';
+import { BiLoader, BiSearch } from 'react-icons/bi';
+import useHeroesStore from '@/store/heroesStore';
 
 export default function SearchBar() {
+  const { heroes, filterHeroes } = useHeroesStore();
+  const [isLoading, setIsLoading] = useState(false);
+  const inputValue = useRef<HTMLInputElement>(null);
+  const debounce = useRef<NodeJS.Timeout | null>(null);
+
+  const handleSearch = () => {
+    if (debounce.current) {
+      clearTimeout(debounce.current);
+    }
+
+    setIsLoading(true);
+
+    debounce.current = setTimeout(() => {
+      filterHeroes(
+        heroes.filter((hero) =>
+          hero.name
+            .toLowerCase()
+            .includes(
+              (inputValue.current as HTMLInputElement).value.toLowerCase(),
+            ),
+        ),
+      );
+      setIsLoading(false);
+    }, 1000);
+  };
+
   return (
     <form className="input-group w-full gap-4" role="searchbox">
       <input
@@ -9,9 +38,18 @@ export default function SearchBar() {
         placeholder="Batman, Superman etc..."
         className="input input-bordered w-full text-sm transition-shadow duration-200 ease-in-out placeholder:text-sm placeholder:text-[#16160a20] focus:shadow-comic focus:outline-none md:w-1/2 md:text-sm md:placeholder:text-sm"
         role="search-input"
+        ref={inputValue}
+        onChange={handleSearch}
       />
-      <button className="btn btn-primary h-12 w-12 rounded-full shadow-comic md:shadow-none md:hover:shadow-comic">
-        <BiSearch className="text-2xl" />
+      <button
+        className="btn btn-primary h-12 w-12 rounded-full shadow-comic md:shadow-none md:hover:shadow-comic"
+        onClick={handleSearch}
+      >
+        {isLoading ? (
+          <BiLoader className="text-2xl" />
+        ) : (
+          <BiSearch className="text-2xl" />
+        )}
       </button>
     </form>
   );
