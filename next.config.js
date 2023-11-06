0092be6a08649efa1f43fa01a6e772cce3000c65
fnandesrafael <fnandesrafael@pm.me>
2023-11-06 09:32:51 -0300

    fix: deploy from vercel to netlify

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
       }
     ]
   },
-  crossOrigin: 'use-credentials'
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Archivo_Black, Poppins } from 'next/font/google';
 import './globals.css';
-import Head from 'next/head';
 
 const archivoBlack = Archivo_Black({
   weight: '400',
@@ -31,12 +30,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="retro">
-      <Head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
       <body
         className={`${poppins.variable} ${archivoBlack.variable} text-[#16160A]`}
       >
